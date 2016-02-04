349d638987652ea4666ba18065178943b48c8789
marshville <marshville@users.noreply.github.com>
2016-02-04 15:55:49 +0000

    Change greeting

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 var port = process.env.PORT || 3000;
 http.createServer(function(req,res){
 	res.writeHead(200, {'Content-Type': 'text/plain' });
-	res.end('Hello World from Mikes\n');
+	res.end('Hello World from Marshy\n');
 }).listen(port);
