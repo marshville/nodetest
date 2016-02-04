72bef7c8f456c423170538ea8ba6c65014c51079
marshville <marshville@users.noreply.github.com>
2016-02-04 13:26:17 +0000

    Remove Debug
    Temporary comment out of debug

--- helloDebug.js
+++ helloDebug.js
@@ -1,7 +1,7 @@
-var debug = require('debug')('hello');
+// var debug = require('debug')('hello');
 var http = require('http');
 var port = process.env.PORT || 3000;
-debug("This should never appear unless DEBUG=hello");
+// debug("This should never appear unless DEBUG=hello");
 http.createServer(function(req,res){
 	res.writeHead(200, {'Content-Type': 'text/plain' });
 	res.end('Hello World from Mikes\n');
