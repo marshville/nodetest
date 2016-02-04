f2d1d5dd3a61955d1006553ebde4186af6ec4903
marshville <marshville@users.noreply.github.com>
2016-02-04 17:05:44 +0000

    Added Get Persons

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ var express = require('express');
 var debug = require('debug')('app');
 // Create express instance
 var app = express();
+
+// Create a person database
+var personData = [
+  {
+    "id": 1,
+    "firstName": "Ted",
+    "lastName": "Neward",
+    "status": "MEANing"
+  },
+  {
+    "id": 2,
+    "firstName": "Brian",
+    "lastName": "Randell",
+    "status": "TFSing"
+  }
+];
+
+var getAllPersons = function(req,res) {
+	var response = personData;
+	res.send(JSON.stringify(response))
+}
+app.get('/persons', getAllPersons);
+
 // Set up a simple route
 app.get('/', function (req, res) {
   debug("/ requested");
@@ -16,3 +39,4 @@ var server = app.listen(port, function () {
   var port = server.address().port;
   console.log('Example app listening at http://%s:%s', host, port);
 });
+
